f9ba7895e6ae3a4502141f957ad4474904109572
ntphu <ntphu@DESKTOP-8PTPDEU.local>
2020-04-11 11:51:21 -0300

    Update BookCtrl.js, Author.js, and 5 more files...

--- nodejs/controllers/BookCtrl.js
+++ nodejs/controllers/BookCtrl.js
@@ -1,5 +1,6 @@
-const {Book, Author, Genre} = require('../models/indexModel');
+const {BookSchema, AuthorSchema, GenreSchema} = require('../models/indexModel');
 const Response = require('./Response');
+const mongoose = require('mongoose');
 
 exports.getList = async function(req, res){
         await Book.find({}, "title summary")
@@ -9,3 +10,24 @@ exports.getList = async function(req, res){
                     Response(res, err, data)
             })
 }
+
+class BookCtrl extends BaseController {
+        constructor(){
+                super('Book', BookSchema);
+                this.modelList = mongoose.modelNames();
+                console.log(this.modelList);
+        }
+
+        async getListFull(req, res){
+                let Author = this.modelList['author'];
+                let Genre = this.modelList['genre'];
+                await super._model.find({}, 'title summary')
+                        .populate({path: 'author', model: Author})
+                        .populate('genre', Genre)
+                        .exec((err, data)=>{
+                                Response(res, err, data)
+                        })
+        }
+}
+
+module.exports = BookCtrl;
