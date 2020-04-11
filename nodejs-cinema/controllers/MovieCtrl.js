b32ad09b6a8af27aa9192b0ec178d38d4aaecae6
ntphuong2603 <ntphuong2603@gmail.com>
2020-04-11 18:47:02 -0300

    modify the create method

--- nodejs-cinema/controllers/MovieCtrl.js
+++ nodejs-cinema/controllers/MovieCtrl.js
@@ -1,14 +1,49 @@
 const Movie = require('../models/Movie');
 
+function response(res, err, data){
+    let code = 200;
+    if (err){
+        code = 400;
+    }
+    res.status(code).json({
+        messeage: err,
+        data: data
+    })
+}
+
 exports.getList = async function(req, res){
     await Movie.find({},(err, data)=>{
-        let code = 200;
-        if (err){
-            code = 400;
-        }
-        res.status(code).json({
-            messeage: err,
-            data: data
-        })
+        response(res, err, data);
+    })
+}
+
+exports.create = async function(req, res){
+    const movie = req.body;
+    console.log(movie);
+    await Movie.create(movie, (err, data)=>{
+        response(res, err, data);
+        console.log(data);
+    });
+}
+
+exports.read = async function(req, res){
+    const id = req.params.id;
+    await Movie.findById({_id : id}, (err, data)=>{
+        response(res, err, data);
+    })
+}
+
+exports.update = async function(req, res){
+    const id = req.params.id;
+    const movie = new Movie(req.body);
+    await Movie.findByIdAndUpdate({_id: id, movie}, (err, data)=>{
+        response(res, err, data);
+    })
+}
+
+exports.delete = async function(req, res){
+    const id = req.params.id;
+    await Movie.findByIdAndDelete({_id:id}, (err, data)=>{
+        response(res, err, data);
     })
 }
--- nodejs-cinema/routers/MovieRouter.js
+++ nodejs-cinema/routers/MovieRouter.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const MovieCtrl = require('../controllers/MovieCtrl');
 
 router.get('/all', MovieCtrl.getList);
+router.get('/:id', MovieCtrl.read);
+router.post('/create', MovieCtrl.create);
+router.post('/update/:id', MovieCtrl.update);
+router.delete('/:id', MovieCtrl.delete);
 
 module.exports = router;
