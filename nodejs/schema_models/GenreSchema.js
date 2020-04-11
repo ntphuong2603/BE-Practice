2ea8a17bac5d941594e63d0be74539326e68f567
ntphuong2603 <ntphuong2603@gmail.com>
2020-04-11 15:20:25 -0300

    Delete folder nodejs

--- nodejs/schema_models/GenreSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const GenreShema = new Schema({
-    name: {type: String, required: true},
-})
-
-//module.exports = mongoose.model('Genre', GenreShema);
-module.exports = GenreShema;
