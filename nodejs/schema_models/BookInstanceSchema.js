f9b4d47bff011667edee63f7c705d7fb953a8b3f
ntphuong2603 <ntphuong2603@gmail.com>
2020-04-11 15:01:38 -0300

    Submit model schema

---
+++ nodejs/schema_models/BookInstanceSchema.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+//const Schema = mongoose.Schema;
+
+var BookInstanceSchema = new mongoose.Schema({
+    book: { type: mongoose.Schema.Types.ObjectId, ref: 'Book', required: true }, //reference to the associated book
+    imprint: {type: String, required: true},
+    status: {type: String, required: true, enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'], default: 'Maintenance'},
+    due_back: {type: Date, default: Date.now}
+})
+
+//module.exports = mongoose.model('BookInstance', BookInstanceSchema);
+module.exports = BookInstanceSchema;
---
+++ nodejs/schema_models/indexModel.js
@@ -0,0 +1,11 @@
+const BookSchema = require('./BookSchema');
+const AuthorSchema = require('./AuthorSchema');
+const GenreSchema = require('./GenreSchema');
+const BookInstanceSchema = require('./BookInstanceSchema');
+
+module.exports = {
+    BookSchema: BookSchema,
+    AuthorSchema: AuthorSchema,
+    GenreSchema: GenreSchema,
+    BookInstanceSchema: BookInstanceSchema
+}
