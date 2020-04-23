bd134e73e391fca3dc41a630c5e6c04c05bc4aea
ntphuong2603 <ntphuong2603@gmail.com>
2020-04-23 20:59:26 -0300

    Push to Heroku 2nd time

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+const port = 5000;
+app.listen(port, ()=>{
+    console.log(`NodeJS is listenning on ${port}`);
+})
+
+app.get('/', (req, res)=> {
+    res.send(' MongoDB - Cinema');
+})
+
+const db = require('./connection/mongoDB');
+
+const MovieRouter = require('./routers/MovieRouter');
+app.use('/movie', MovieRouter);
