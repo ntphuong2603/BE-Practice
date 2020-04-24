8c37e4ef9401ac6d263c7e1c82f46b0717c8fd53
ntphuong2603 <ntphuong2603@gmail.com>
2020-04-24 10:40:33 -0300

    change index.js

--- index.js
+++ index.js
@@ -2,21 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const app = express();
+let app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-const port = process.env.port || 5000;
+let db = require('./connection/mongoDB');
+
+const port = process.env.PORT || 8080;
 app.listen(port, ()=>{
-    console.log(`NodeJS is listenning on ${port}`, app.settings.env);
+    let server = app.address().port;
+    console.log(`NodeJS is listenning on ${server}`, app.settings.env);
 })
+
+let MovieRouter = require('./routers/MovieRouter');
+app.use('/movie', MovieRouter);
 
 app.get('/', (req, res)=> {
-    res.send(' MongoDB - Cinema');
+    res.status(200).json(' MongoDB - Cinema');
 })
-
-const db = require('./connection/mongoDB');
-
-const MovieRouter = require('./routers/MovieRouter');
-app.use('/movie', MovieRouter);
