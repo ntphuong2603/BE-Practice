94c55a1db0f05578ceb83e596945927de825e854
ntphuong2603 <ntphuong2603@gmail.com>
2020-04-24 12:48:59 -0300

    delete some folders & files

--- models/Movie.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const MovieSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    rating: {type: Number, required: true},
-    time: {type: [String]}
-})
-
-module.exports = mongoose.model('Movie', MovieSchema);
